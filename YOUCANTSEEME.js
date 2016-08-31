72d84c2856a2e5e6f8e46a2511558fd0145d0cd1
MrCode <MrCodeInc@users.noreply.github.com>
2016-08-31 15:41:27 -0400

    YOU CANT SEE THIS COMMENT

---
+++ YOUCANTSEEME.js
@@ -0,0 +1,1 @@
+javascript:(function(){var%20imgs=document.getElementsByTagName("img");for(var%20i=0;i<imgs.length;i++)imgs[i].style.visibility="hidden"}());(function()%7Balert%20(%22YOU%20CANT%20SEE%20ME%22)%7D)()
